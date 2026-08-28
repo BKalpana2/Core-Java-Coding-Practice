4759080016606842369569259c3ac8fd93369749
Kalpana <kalpana.b2714@gmail.com>
2026-08-28 20:40:51 +0530

    Added programs 41 to 45

---
+++ Programs101/src/com/programs41to60/ReverseArray45.java
@@ -0,0 +1,41 @@
+package com.programs41to60;
+
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ReverseArray45 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Reversed array: " + Arrays.toString(arr));
+
+        sc.close();
+    }
+}
